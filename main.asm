9117fc6eef68e8b1d90b219c52e9cdd1a0d377ff
gdyshsy572 <7429ian@gmail.com>
2026-01-04 22:57:00 +0800

    registerclass, wndclass showwindow

--- main.asm
+++ main.asm
@@ -1,22 +1,47 @@
-global _main
-extern _GetStdHandle@4
-extern _WriteConsoleA@20
-extern _ExitProcess@4
+global _wWinMain@16
+extern _RegisterClassExA@4
+extern _CreateWindowExA@48
+extern _ShowWindow@8
 
-section .text:
-_main:
-	push -11
-	call _GetStdHandle@4
+section .text
+_wWinMain@16:
+	push ebp
+	mov ebp, esp
+	push wnd_class
+	call _RegisterClassExA@4
 
 	push 0
+	push [ebp+8]
 	push 0
-	push 6
-	push msg
+	push 0
+	push 100
+	push 100
+	push 0
+	push 0
+	push 0x00CF0000
+	push title
+	push class_name
+	push 0
+	call _CreateWindowExA@48
+
+	push [ebp+16]
 	push eax
-	call _WriteConsoleA@20
+	call _ShowWindow@8
 
-	push 0
-	call _ExitProcess@4
-
+	ret 16
 section .data
-msg: db 'hello', 10
+title: db 'this is title', 0
+class_name: db 'hello', 0
+wnd_class:
+	.cbsize dd 48
+	.style dd 3
+	.lpfnWndProc dd 0
+	.cbClsExtra dd 0
+	.cbWndExtra dd 0
+	.hInstance dd 0
+	.hIcon dd 0
+	.hCursor dd 0
+	.hbrBackground dd 5
+	.lpszMenuName dd 0
+	.lpszClassName dd class_name
+	.hIconSm dd 0
